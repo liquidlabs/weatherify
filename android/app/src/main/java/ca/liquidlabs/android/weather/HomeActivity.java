975f5e977cc564ca009eb7793f5023168ce4fc94
Abdullah Rubiyath <itsalif@gmail.com>
2014-03-09 23:15:09 -0400

    targetSDK changed to 14 (4.0+) Nav Drawer added with an no fragment in the Home Activity Dummy Datas added - (Array Adapters needs to be replaced with Custom Adapter)

--- android/app/src/main/java/ca/liquidlabs/android/weather/HomeActivity.java
+++ android/app/src/main/java/ca/liquidlabs/android/weather/HomeActivity.java
@@ -2,36 +2,124 @@ package ca.liquidlabs.android.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+
+/**
+ * HomeActivity - contains the Drawer along with ViewPagers
+ *
+ * @author Abdullah Rubiyath
+ */
 
 public class HomeActivity extends Activity {
+
+    /**
+     * drawer layout
+     */
+    private DrawerLayout mDrawerLayout;
+
+    /**
+     * drawer toggle
+     */
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    /**
+     * drawer list - ListView
+     */
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        setupDrawer();
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
+    /**
+     * Sets up the Drawer's Toggle and List View
+     */
+    public void setupDrawer() {
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_launcher, R.string.app_name, R.string.app_name) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        initListView();
+    }
+
+
+
+    /**
+     * Initializes the ListView of Drawer
+     * @TODO: improve with a custom Adapter extending from BaseAdapter
+     */
+    private void initListView()
+    {
+        String[] titles = {
+                "Home", "Max", "Contact", "About"
+        };
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, titles));
+    }
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            mDrawerLayout.closeDrawer(mDrawerList);
+        }
+    }
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
+
+        // Handle your other action bar items...
         return super.onOptionsItemSelected(item);
     }
 
